3ead66ff23954e8d1111e8455f513ee569888b1b
Guilherme Machado <guifermach@hotmail.com>
2023-02-01 19:37:06 -0300

    RecuperarFilmes/RecuperarFilmePorId

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -9,12 +9,26 @@ namespace FilmesAPI.Controllers
     {
 
         private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
 
         [HttpPost]
         public void AdicionarFilme([FromBody]Filme filme)
-        {           
+        {
+            filme.Id = Id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+           return filmes.FirstOrDefault(filme => filme.Id == id);
         }
 
     }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -12,6 +12,6 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1,1000, ErrorMessage = "A duração deve ter o minimo de 1 e no maximo 600 minutos")]
         public double Duracao { get; set; }
-
+        public int Id { get; internal set; }
     }
 }
